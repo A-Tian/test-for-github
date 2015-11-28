e6844eaf8c13ceb3ba1c7af5aea7ab82f05c0e66
Shu-Tian Lin <atian1129@gmail.com>
2015-11-28 23:13:58 +0800

    finish simple math

--- Simple math/main.cpp
+++ Simple math/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <math.h>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
+    int a,b;
 
+    cout<<"please enter two number:\n";
+    cin>>a>>b;
+    cout<<a<<"+"<<b<<"="<<a+b<<endl;
+    cout<<a<<"-"<<b<<"="<<a-b<<endl;
+    cout<<a<<"*"<<b<<"="<<a*b<<endl;
+    cout<<a<<"/"<<b<<"="<<a/b<<"..."<<a%b<<endl;
 }
